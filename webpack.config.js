ced232896445598c03cdfda74fa3237c8b1d5d8e
mike-stumpf <mike.a.stumpf@gmail.com>
2017-01-04 10:48:38 +0000

    core - now able to serve fonts and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,64 @@
-var debug = process.env.NODE_ENV !== "production";
+var debug = process.env.NODE_ENV !== 'production';
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
-  module: {
-    loaders: [
-      {
-        // Load all jsx react files and put into 1 source file
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
-      },
-      {
-        // compile scss. NOTE: for dev, this is a virtual file in memory, not created as an actual file on file system
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass')
-      },
-      {
-        // uses url for loading up to 10k, otherwise uses file loader
-        test: /\.(png|jpg)$/,
-        loader: 'url-loader?limit=10000'
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
-  },
-  plugins: debug ? [
-      // In dev, this is created as a virtual file to be loaded on the webpack dev server
-      new ExtractTextPlugin('style.min.css', { allChunks:true })
+    context: path.join(__dirname, 'src'),
+    devtool: debug ? 'inline-sourcemap' : null,
+    entry: './js/client.js',
+    module: {
+        loaders: [
+            {
+                // Load all jsx react files and put into 1 source file
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+                }
+            },
+            {
+                // compile scss. NOTE: for dev, this is a virtual file in memory, not created as an actual file on file system
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('css!sass')
+            },
+            {
+                // test: /\.(woff2|woff|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                test: /\.(woff2|woff|ttf|eot|svg)(\?.*$|$)/,
+                loader: 'file-loader?name=fonts/[name].[ext]',
+                include: [
+                    path.join(__dirname, 'src'),
+                    path.join(__dirname, 'node_modules')
+                ]
+            },
+            {
+                // test: /\.(jpg|jpeg|gif|png|ico)$/,
+                test: /\.(jpg|jpeg|gif|png|ico)(\?.*$|$)$/,
+                loader: 'file-loader?name=img/[name].[ext]',
+                include: [
+                    path.join(__dirname, 'src'),
+                    path.join(__dirname, 'node_modules')
+                ]
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/src/',
+        filename: 'client.min.js'
+    },
+    plugins: debug ? [
+        // In dev, this is created as a virtual file to be loaded on the webpack dev server
+        new ExtractTextPlugin('style.min.css', { allChunks:true })
     ] : [
-      new webpack.optimize.DedupePlugin(),
-      new webpack.optimize.OccurenceOrderPlugin(),
-      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-      new ExtractTextPlugin('style.min.css', { allChunks:true })
-  ],
-  devServer: {
-    // needed so that all urls go to index
-    historyApiFallback: true,
-  }
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+        new ExtractTextPlugin('style.min.css', { allChunks:true })
+    ],
+    devServer: {
+        // needed so that all urls go to index
+        historyApiFallback: true,
+    }
 };
