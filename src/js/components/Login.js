f702b0b4352de6a71a14177b817ba4515427a934
mike-stumpf <mike.a.stumpf@gmail.com>
2017-01-04 11:39:36 +0000

    header - added state to the nav menu; added dropdown menu styling; added default button styling

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -6,108 +6,108 @@ import { baseWEBUrl } from "../config.js"
 import { createLoginuser,loginLoginuser, fetchLoginuser, logoutLoginuser } from "../actions/loginuserActions"
 
 @connect((store) => {
-  return {
-    loginuser: store.loginuser.loginuser,
-    error: store.loginuser.error
-  };
+    return {
+        loginuser: store.loginuser.loginuser,
+        error: store.loginuser.error
+    };
 })
 
 export default class Layout extends React.Component {
-  componentWillMount() {
-    this.props.dispatch(fetchLoginuser())
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const wasNotLoggedIn = !this.props.loginuser.id;
-    const isLoggedIn = !(!nextProps.loginuser.id);
-    if(wasNotLoggedIn && isLoggedIn) {
-      this.props.router.push('/users');
+    componentWillMount() {
+        this.props.dispatch(fetchLoginuser())
     }
-  }
 
-
-  /// Handlers
-  createLoginuser() {
-    this.props.dispatch(createLoginuser())
-  }
-  
-  logoutLoginuser(e) {
-    this.props.dispatch(logoutLoginuser())
-  }
-
-  loginLoginuser(e) {
-    e.preventDefault();
-    let loginData = {
-      email: this.email.value,
-      password: this.password.value
-    };
-    this.props.dispatch(loginLoginuser(loginData))
-  }
-
-
-  render() {
-
-    // TODO: move this to a helper class
-    const addFullNameForUser = (user) => {
-        if (user.name) {
-          user.fullName = user.name;
+    componentWillReceiveProps(nextProps) {
+        const wasNotLoggedIn = !this.props.loginuser.id;
+        const isLoggedIn = !(!nextProps.loginuser.id);
+        if(wasNotLoggedIn && isLoggedIn) {
+            this.props.router.push('/users');
         }
-        else if(user.first_name && user.last_name) {
-          user.fullName= user.first_name + ' ' + user.last_name;
-        } else if (user.first_name) {
-            user.fullName = user.first_name;
+    }
 
+
+    /// Handlers
+    createLoginuser() {
+        this.props.dispatch(createLoginuser())
+    }
+
+    logoutLoginuser(e) {
+        this.props.dispatch(logoutLoginuser())
+    }
+
+    loginLoginuser(e) {
+        e.preventDefault();
+        let loginData = {
+            email: this.email.value,
+            password: this.password.value
+        };
+        this.props.dispatch(loginLoginuser(loginData))
+    }
+
+
+    render() {
+
+        // TODO: move this to a helper class
+        const addFullNameForUser = (user) => {
+            if (user.name) {
+                user.fullName = user.name;
+            }
+            else if(user.first_name && user.last_name) {
+                user.fullName= user.first_name + ' ' + user.last_name;
+            } else if (user.first_name) {
+                user.fullName = user.first_name;
+
+            } else {
+                user.fullName = user.last_name;
+            }
+
+            return user;
+        };
+
+        let { loginuser, error } = this.props;
+        loginuser = addFullNameForUser(loginuser);
+        const name=<h1>{loginuser.fullName}</h1>;
+        let createUserButtons='';
+
+        function ErrorBlock(props) {
+            if(props.error) {
+                let message='';
+
+                if(props.error.status===400) {
+                    message ='Failed to login. Please make sure your credentials are correct.';
+                } else if (props.error.status===401) {
+                    message ='Please Log in';
+                }else {
+                    message = 'Failed to login. Please try again later. If you continue to have problems, please contact support.';
+                }
+
+                return <div className="errors">{message}</div>;
+            } else {
+                return <div></div>
+            }
+        };
+
+        if (!loginuser.id) {
+            createUserButtons =<div>
+                <form role="form">
+                    <div className="form-group">
+                        <input ref={(input) => { this.email = input; }} type="email"  placeholder="Email" />
+                        <input ref={(input) => {this.password = input;}} type="password"  placeholder="Password" />
+                    </div>
+                    <button onClick={this.loginLoginuser.bind(this)}>login user</button>
+                </form>
+                <button onClick={this.createLoginuser.bind(this)}>create user</button>
+                <ErrorBlock error={error} />
+                <div>
+                    Forgot password? <a href={baseWEBUrl+"/reset"}>Click Here</a>
+                </div>
+            </div>;
         } else {
-          user.fullName = user.last_name;
+            createUserButtons =<div>
+                <button onClick={this.logoutLoginuser.bind(this)}>logout user</button>
+            </div>;
         }
 
-        return user;
-      };
-
-    let { loginuser, error } = this.props;
-    loginuser = addFullNameForUser(loginuser);
-    const name=<h1>{loginuser.fullName}</h1>;
-    let createUserButtons='';
-
-    function ErrorBlock(props) {
-      if(props.error) {
-        let message='';
-
-        if(props.error.status===400) {
-          message ='Failed to login. Please make sure your credentials are correct.';
-        } else if (props.error.status===401) {
-          message ='Please Log in';
-        }else {
-         message = 'Failed to login. Please try again later. If you continue to have problems, please contact support.';
-        }
-
-        return <div className="errors">{message}</div>;
-      } else {
-        return <div></div>
-      }
+        return <div>{name}{createUserButtons}</div>;
     };
-
-    if (!loginuser.id) {
-      createUserButtons =<div>
-        <form role="form">
-          <div className="form-group">
-            <input ref={(input) => { this.email = input; }} type="email"  placeholder="Email" />
-            <input ref={(input) => {this.password = input;}} type="password"  placeholder="Password" />
-          </div>
-          <button onClick={this.loginLoginuser.bind(this)}>login user</button>
-        </form>
-        <button onClick={this.createLoginuser.bind(this)}>create user</button>
-        <ErrorBlock error={error} />
-        <div>
-          Forgot password? <a href={baseWEBUrl+"/reset"}>Click Here</a>
-        </div>
-      </div>;
-    } else {
-      createUserButtons =<div>
-          <button onClick={this.logoutLoginuser.bind(this)}>logout user</button>        
-        </div>;
-    }
-
-    return <div>{name}{createUserButtons}</div>;
-  };
 }
--- src/js/components/layout/Nav.js
+++ src/js/components/layout/Nav.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { connect } from "react-redux"
 import { IndexLink, Link } from "react-router";
+
+import { fetchLoginuser, logoutLoginuser } from "../../actions/loginuserActions"
+
+@connect((store) => {
+    return {
+        loginuser: store.loginuser.loginuser
+    };
+})
 
 export default class Nav extends React.Component {
     constructor() {
@@ -7,22 +16,50 @@ export default class Nav extends React.Component {
         this.state = {};
     }
 
-/// Nav for all pages
-    render() {
-        const { name, loggedIn } = this.props;
-        return (
-            <nav id="header-menu-container" role="navigation" class="col-sm-8">
-                <ul>
+    componentWillMount() {
+        this.props.dispatch(fetchLoginuser())
+    }
+
+    //listeners
+    logoutLoginuser(e) {
+        this.props.dispatch(logoutLoginuser())
+    }
+
+    //templates
+    renderNavMenu(){
+        const { loginuser } = this.props;
+        if (loginuser.id) {
+            return (
+                <ul class="standard-menu">
                     <li>
-                        {name}{loggedIn}
+                        {loginuser.first_name} ({loginuser.role})
+                        <ul class="sub-menu">
+                            <li>
+                                <Link to="users">Users</Link>
+                            </li>
+                            <li>
+                                <IndexLink to="/" onClick={this.logoutLoginuser.bind(this)}>Logout</IndexLink>
+                            </li>
+                        </ul>
                     </li>
+                </ul>
+            );
+        } else {
+            return (
+                <ul>
                     <li>
                         <IndexLink to="/">Login</IndexLink>
                     </li>
-                    <li>
-                        <Link to="users">Users</Link>
-                    </li>
                 </ul>
+            );
+        }
+    }
+
+/// Nav for all pages
+    render() {
+        return (
+            <nav id="header-menu-container" role="navigation" class="col-sm-8">
+                { this.renderNavMenu() }
             </nav>
         );
     }
