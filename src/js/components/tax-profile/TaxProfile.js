77fc6fe87401907c718b638d23a5f4180197664a
mike-stumpf <mike.a.stumpf@gmail.com>
2017-01-09 10:58:23 +0000

    tax profile - styled tax profile; added back font awesome

--- src/js/components/tax-profile/TaxProfile.js
+++ src/js/components/tax-profile/TaxProfile.js
@@ -6,12 +6,23 @@ import { Link  } from "react-router"
 import Sidebar from "../layout/Sidebar";
 
 export default class TaxProfile extends React.Component {
+    
+    //todo, get export urls
+    
     render() {
         return (
             <main class="grid-container row">
                 <Sidebar activeScreen="taxProfile" userId={this.props.params.userId}/>
                 <section class="col-sm-8">
                     <h1>Tax Profile</h1>
+                    <div>
+                        <a class="tax-profile-export">
+                            <i class="fa fa-file-excel-o"></i>Export CSV
+                        </a>
+                        <a class="tax-profile-export">
+                            <i class="fa fa-file-pdf-o"></i>Export PDF
+                        </a>
+                    </div>
                 </section>
             </main>
         )
